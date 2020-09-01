675edffbf330aa5a60ad2651026f3190ed9827b1
beer <614259006@webmail.npru.ac.th>
2020-09-01 15:54:36 +0700

    Create Project Structure

---
+++ Calculator.php
@@ -0,0 +1,15 @@
+<?php
+class Calculator{ //Calculator number
+    public function multiply($num1,$num2){
+        return $num1 * $num2;
+    }
+        public function add($num1,$num2){
+            return $num1 + $num2;
+        }
+            public function subtract($num1,$num2){
+                return $num1 - $num2;
+            }
+            public function divide($num1,$num2){
+                return $num1 / $num2;
+            }
+}
